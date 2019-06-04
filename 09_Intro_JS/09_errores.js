43671aad377e569a7919fdd48155eda5bbcc8271
Evelin Zarka <evelinzarka@gmail.com>
2019-06-04 12:31:16 +0200

    JS 4 Junio

--- 09_Intro_JS/09_errores.js
+++ 09_Intro_JS/09_errores.js
@@ -1,68 +1,70 @@
-//COPIAR CODIGO DE PROFESOR!!!!!
 
+/** Separación de responsabilides */
 
-
-/******** Separación de responsabilidades *********/
-
-//mejor ponerlo en dos funciones las dos acciones : comprobar edad y devolver el mensaje >>> funciones puras
-function codificaEdad (edad) {
-    let i = 3 
-    if (!isValidNumber(edad) {
-        i = -2
-    } else if (edad < 18) {
-        i = 0
-    } else if (edad < 35) {
-        i = 1
-    } else if (edad < 50) {
-        i = 2 
-    }
-    return i 
-}
-
-function ResponderEdad (edad) {
-    let mensajes = [
-        'eres menor de edad' ,
-        'eres joven' ,
-        'cuidaooo' ,
-        'sorry for you'
-    ]
-    let i = codificaEdad(edad)
-    if (i >= 0 && i <= 3 ) {
-        console.log(`Con ${edad} años`, mensajes[i] ) 
-    } else {
-        console.log('Error' ${i})
-    }
-}
-
-let e = 53
-ResponderEdad(e)
-
-//Comprobar si un numero es válido
-function isValidNumber(e) {
-    r = true
-    if ( isNaN(e) || typeof n == 'boolean'
-    || Array.isArray(e) ) {
+function isValidNumber(n) {
+    let r = true
+    if ( isNaN(n) || typeof n == 'boolean' 
+    || Array.isArray(n) || n === ''  || n === null) {
         r = false
     }
     return r
 }
 
-function probar () {
-   e = 25
-e = 34
-e = '46'
-e = '34 pepes'
-e =  true
-e =  false
-e = undefined
-e = []
-e = {}
-e = null 
+function codificarEdad (edad) { //mejor ponerlo en dos funciones las dos acciones : comprobar edad y devolver el mensaje >>> funciones puras
+    let i = 3
+    if (!isValidNumber(edad)) {
+        i = -2
+    } else if (edad <= 0) {
+        i = -1
+    } else if (edad < 18) {
+        i = 0
+    } else if (edad < 35) {
+        i = 1
+    } else if (edad < 50) {
+        i = 2
+    }
+    return i
 }
 
-probar ()
+function responderSegunEdad (edad) {   
+    let mensajes = [
+        'eres menor de edad',
+        'eres joven',
+        'empieza a temer cuidado', 
+        'lo siento por ti'
+    ]
+    let errores = [
+        'Error: El número indicado no es válido',
+        'Error: La edad indicada no es un número'
+    ]
+    let i = codificarEdad(edad)
+    if (i >= 0 && i <= 3) {
+        console.log(`Con ${edad} años`, mensajes[i])
+    } else {
+        console.log(errores[-i-1])
+    }
+}
 
-
-
-isValidNumber(e)
-console.log(e, isValidNumber(e))
+function probar() {
+    let e = 2
+    e = 18
+    e = 25
+    e = 35
+    e = 45
+    e = 55
+    e = 65
+    e = 0
+    e = -3
+     e = '34'
+    e = 'Pepe'
+    e = true
+    e = false
+    e = {}
+    e = []
+    e = undefined
+    e = ''
+    e = null
+    e = NaN
+    responderSegunEdad(e)
+}
+probar()
