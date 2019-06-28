8769d5ca8c8dfa5e557ecc76d19478acf26dcc0f
ezrk <evelinzarka@gmail.com>
2019-06-28 17:03:36 +0200

    28 06 1

---
+++ JS_OOP/01_objetos.js
@@ -0,0 +1,17 @@
+'use strict'
+
+const obj = new Object()
+// es lo mismo que: 
+const persona = {
+    nombre: 'Pepe',
+    edad: 35;
+}
+
+const animal1 = {
+    nombre: 'Rufo',
+    edad: 3
+}
+
+animal1.peso = 12
+
+console.log(animal1)
