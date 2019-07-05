55854e0efd4dab41fbe5bbb15cc099a4a696c1da
ezrk <evelinzarka@gmail.com>
2019-07-05 21:17:20 +0200

    5 junio 2

---
+++ JS_OOP/20_catalogo.js
@@ -0,0 +1,9 @@
+//Ejercicio 20 Apuntes JS
+
+//3 propiedades + 4.propiedad (iva base) si queremos 
+
+//2 metodos
+//iva -> return this.precio *0,21
+//mostrar a1.mostrar
+
+//funcion constructora: varios artículos consta1 = new Articulo()
