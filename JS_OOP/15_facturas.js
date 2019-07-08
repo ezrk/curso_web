b18edd3a1eff71cd6576de7c35a88223969f7314
Evelin Zarka <evelinzarka@gmail.com>
2019-07-08 13:19:02 +0200

    8 junio mñn

--- JS_OOP/15_facturas.js
+++ JS_OOP/15_facturas.js
@@ -16,7 +16,7 @@ Las facturas están formadas por:
     - precio por unidad
     - cantidad 
 - otra información básica de la factura
-    - el número de factura
+    - el número de tactura
     - tipo de IVA
     - forma de pago
     - la fecha
@@ -26,80 +26,118 @@ Añadir al menos dos métodos:
     o al menos el importe total de la factura.
 */
 
+const {fechaEsp, fijarLongitudCadena} = require('./15_helpers.js')
 
-const datosEmpresa = {
-    nombre: 'Pepephone',
-    direccion : {
-        calle: 'Calle Alcalá 1.',
-        ciudad: 'Madrid',
-        provincia: 'Madrid'
+const templateFactura = {
+    empresa: {
+        nombre: '',
+        direccion: '',
+        telefono: '',
+        nif: '',
     },
-    teléfono: '91 222 333',
-    NIF: 'A29268166. B.' 
-}
-
-const datosCliente = {
-    nombre: 'José Manuel Pérez',
-    direccion : {
-        calle: 'Bonito, 1',
-        ciudad: 'Zahara de los Atunes',
-        provincia: 'Cádiz'
+    cliente: {
+        nombre: '',
+        direccion: '',
+        telefono: '',
+        nif: '',
     },
-    teléfono: '91 555 333',
-    NIF: 'A29268255. B.' 
-}
-
-
-const listaCompra = {
-    descripcion: '',
-    preciounidad: price ,
-    cantidad: ud ,
-
-}
-
-const datosFactura = {
-    nfactura: ,
-    iva: 16,
-    formapago: ,
-    fecha: ,
-
-}
-
-//Fecha
-var f = new Date();
-document.write(f.getDate() + "/" + (f.getMonth() +1) + "/" + f.getFullYear());
-
-
-const factura1 = new Factura();
-const factura1 = [datosFactura, datosEmpresa, datosCliente, listaCompra]
-
-
-/** function importeTotal
- * @description calcula importe total de la factura
- * @param {number} price, ud, iva
- * @returns {number}
- */
-
-
- /** function mostrarFactura
- * @description mostrar los detalles de la factura
- * @param {number, string} 
- * @returns {number, string}
- */
- 
-function mostrarFactura(datosFactura, datosEmpresa, datosCliente, listaCompra) {
-    var resultado = ``;
-    for (var i in objeto) {
-      //objeto.hasOwnProperty se usa para filtrar las propiedades del objeto
-      if (objeto.hasOwnProperty(i)) {
-          resultado += `${nombreObjeto}.${i} = ${objeto[i]}\n`;
-      }
+    items : [
+        {descripcion: '', precioU: 0, cantidad: 0} 
+    ],
+    numFactura: '',
+    tipoIVA: '',
+    formaPago: '',
+    fecha: new Date()
     }
-    return resultado;
-  }
+
+templateFactura.calcularImporte = function() {}
+templateFactura.mostrarImporte = function() {}
 
 
+const factura = {
+    empresa: {
+        nombre: 'Ediciones Tuatalug',
+        direccion: 'c/ Pez 1, Madrid',
+        telefono: '625875921',
+        nif: 'T-34761234',
+    },
+    cliente: {
+        nombre: 'Librería Boracai',
+        direccion: 'c/ Carranza 5, Cádiz',
+        telefono: '625493421',
+        nif: 'F-76521743',
+    },
+    items : [
+        {descripcion: 'Angular 8.0', precioU: 30, cantidad: 10},   
+        {descripcion: 'JS para torpes', precioU: 32, cantidad: 12},   
+        {descripcion: 'Typescrip Avanzado', precioU: 45, cantidad: 8},   
+        {descripcion: 'Introducción a las aplicaciones Web', precioU: 37, cantidad: 11}],
+    numFactura: '000000012',
+    tipoIVA: 0.04,
+    formaPago: 'Contado',
+    fecha: new Date()
+    }
+
+factura.calcularImporte = function() {
+    const importe = {} 
+    importe.base = 
+        this.items
+        .map( libro => libro.precioU * libro.cantidad)
+        .reduce( (total, item) => total + item ) 
+    importe.iva = importe.base * this.tipoIVA
+    importe.total = importe.base + importe.iva
+    return importe
+}
+
+factura.listarItems = function() {
+    let items = ``
+    this.items.forEach(
+        item => {
+            let desc = fijarLongitudCadena(item.descripcion, 30)
+            let pv = item.precioU
+                .toLocaleString('es', {style: 'currency', currency: 'EUR'})
+            let cant = item.cantidad
+            items += 
+`
+    ${desc}     ${pv}          ${cant}
+`
+    })
+    return items
+}
+
+factura.prepararFactura = function() {
+    const importe = this.calcularImporte()
+    const factura =
+`
+    _______________________________________________________________
+                                        ${this.empresa.nombre}
+                                        ${this.empresa.direccion}
+                                        ${this.empresa.telefono}
+                                        ${this.empresa.nif}
+    Num. Factura: ${this.numFactura}
+    ${this.cliente.nombre}
+    ${this.cliente.direccion}
+    ${this.cliente.telefono}
+    ${this.cliente.nif}
+    Descripción                         Precio      Cantidad
+    _______________________________     _______     ________
+    ${this.listarItems()}
+    Importe ....................................${importe.base
+        .toLocaleString('es', {style: 'currency', currency: 'EUR'})}
+    IVA ........................................${importe.iva
+        .toLocaleString('es', {style: 'currency', currency: 'EUR'})}
+    Importe Total ..............................${importe.total
+        .toLocaleString('es', {style: 'currency', currency: 'EUR'})}
+    Forma de Pago ..............................${this.formaPago}
+    Madrid, ${fechaEsp(this.fecha)}
+    ______________________________________________________________
+`
+    return factura
+}
+
+factura.render = function () {
+    console.log(this.prepararFactura())
+}
 
 
-
-  console.log(factura1)
+factura.render()
--- JS_OOP/20_catalogo.js
+++ JS_OOP/20_catalogo.js
@@ -7,3 +7,43 @@
 //mostrar a1.mostrar
 
 //funcion constructora: varios artículos consta1 = new Articulo()
+
+
+function Producto(nombre, categoria, descripcion, unidades, precio, iva) {
+    this.nombre = nombre
+    this.categoria = categoria
+    this.descripcion = ''
+    this.cantidad = cantidad
+    this.precio = precio
+    this.iva = iva
+    //this.profesion = ''
+    this.mostrarP = function(otro = 'amig@') {
+        let msg = `Hola ${otro} soy ${this.nombre}`
+        console.log(msg)
+    }
+   /*  this.serContratado = function(puesto) {
+        this.profesion = puesto
+    } */
+}
+
+
+const p2 = new Producto('Lavadora LG', 'Lavado', '', 100, 489, 21)
+const p3 = new Producto('Batidora Bosch', 'Cocina','', 150, 60.99, 21)
+
+
+p1.descripcion = 'Lavadora carga frontal - LG F4J7TY1W, Carga de 8 kg, 1400 rpm, Clase A+++(-40%), Blanco'
+p2.descripcion = 'Batidora de mano - Bosch MaxoMixx MSM89160, 1000W, Turbo, Selector 12 velocidades, Silencioso'
+
+
+producto.calcularImporte = function() {
+    const importe = {} 
+    importe.base = 
+        this.items
+        .map( Producto => producto.precio * producto.cantidad)
+        .reduce( (total, item) => total + item ) 
+    importe.iva = importe.base * this.iva
+    importe.total = importe.base + importe.iva
+    return importe
+}
+
+console.log(p1)
